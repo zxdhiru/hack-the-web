612e46ca5eed40ef10d2fa1c2c18e3da3a4921bd
Dhiru <dhiru@alljobguider.in>
2025-12-11 12:15:03 +0530

    Implemented navbard and homepage

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="bg-transparent flex flex-row py-4 px-4 justify-between">
+      {/* Logo */}
+      <h1>Logo.</h1>
+      {/* Menu */}
+      <div className="flex">
+        <ul className="flex justify-between gap-4 ">
+          <li>
+            <Link href={"/events"}>Events</Link>
+          </li>
+          <li>
+            <Link href={"/events"}>Projects</Link>
+          </li>
+          <li>
+            <Link href={"/events"}>Leaderboard</Link>
+          </li>
+        </ul>
+      </div>
+      {/* Button */}
+      <Button className="hidden sm:block">Register</Button>
+    </div>
+  );
+};
+
+export default Navbar;
